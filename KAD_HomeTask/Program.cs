aef79eedf3e8d77db91182173acbb12d0640797a
Artem Kravchenko <drta26@gmail.com>
2019-04-05 18:34:22 +0300

    zadanie2

--- KAD_HomeTask/Program.cs
+++ KAD_HomeTask/Program.cs
@@ -10,6 +10,19 @@ namespace KAD_HomeTask
     {
         static void Main(string[] args)
         {
+            //Задание 1
+
+            int xl;
+            xl = 2;
+            Console.WriteLine(xl);
+
+            //Задание 2
+
+            string str1;
+            Console.WriteLine("Ваше имя?");
+            str1 = Console.ReadLine();
+            string str2="Привет " + str1;
+            Console.WriteLine(str2);
         }
     }
 }
